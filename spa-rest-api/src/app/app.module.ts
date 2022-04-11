86a52db57dc347df825bdf7860391a12ddbd3bbd
ramteju <37100583+ramteju@users.noreply.github.com>
2022-04-11 23:02:33 +0530

    Update app.module.ts

--- spa-rest-api/src/app/app.module.ts
+++ spa-rest-api/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
 
 
@@ -11,6 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './components/grid/grid.component';
 import { BreweriesAddressComponent } from './components/breweries-address/breweries-address.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 @NgModule({
@@ -25,7 +27,9 @@ import { BreweriesAddressComponent } from './components/breweries-address/brewer
     BrowserAnimationsModule,
     HttpClientModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
