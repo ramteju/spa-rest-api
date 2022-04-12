71e069edf596db2ae82a5567dc1d401418059139
ramteju <37100583+ramteju@users.noreply.github.com>
2022-04-13 00:04:01 +0530

    commit

--- spa-rest-api/src/app/components/details-popup/details-popup.component.ts
+++ spa-rest-api/src/app/components/details-popup/details-popup.component.ts
@@ -13,6 +13,16 @@ export class DetailsPopupComponent implements OnInit {
   details:any;
   mapLoaded:any
   options:any;
+  Name:string='';
+  BreweryType:string='';
+  Address_2:string='';
+  StreetAddress:string='';
+  city:string='';
+  State:string='';
+  Country:string='';
+  Postal_Code:string='';
+  PhoneNumber:string='';
+  Web_SiteUrl:string='';
 
   constructor(private restApi:RestApiService, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
@@ -23,6 +33,16 @@ export class DetailsPopupComponent implements OnInit {
 
   async getDetails(){
     console.log(this.data);
+    this.Name = this.data.name;
+    this.BreweryType = this.data.brewery_type;
+    this.Address_2 = this.data.address_2;
+    this.StreetAddress = this.data.address_3;
+    this.city = this.data.city;
+    this.State = this.data.state;
+    this.Country = this.data.country;
+    this.Postal_Code = this.data.postal_code;
+    this.PhoneNumber = this.data.phone_number;
+    this.Web_SiteUrl = this.data.website_url;
     this.options = {
       center: {lat: +this.data.latitude, lng: +this.data.longitude},
       zoom: 4
